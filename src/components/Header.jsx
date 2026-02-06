4da72fe024bf5e0d66514725cda3672d3298c670
Kenneth San Pedro <kennethsanpedro1108@gmail.com>
2026-02-06 14:48:36 +0800

    Refactor modal management: implement modal store with Zustand, update Header component for improved modal handling, and ensure consistent formatting.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,21 +7,35 @@ import {
   SettingOutlined,
 } from "@ant-design/icons";
 import { Tooltip } from "antd";
-import { useModalStore } from "../store/modalStore";
+import useModalStore from "../store/modalStore";
 import { MODALS } from "./modals";
 
 const Header = () => {
-  const {openModal, setModalTitle} = useModalStore();
+  const { openModal, setModalTitle } = useModalStore();
 
   return (
     <div className="w-full h-fit bg-dark-dark-gray flex justify-items-center flex-row gap-3">
-      <button className="text-amber-50 h-full text-base pt-3 pb-1 px-2">Board 1</button>
-      <button className="text-amber-50 h-full text-base pt-3 pb-1 px-2">Board 1</button>
-      <button className="text-amber-50 h-full text-base pt-3 pb-1 px-2">Board 1</button>
-      <button className="text-amber-50 h-full text-base pt-3 pb-1 px-2">Board 1</button>
+      <button className="text-amber-50 h-full text-base pt-3 pb-1 px-2">
+        Board 1
+      </button>
+      <button className="text-amber-50 h-full text-base pt-3 pb-1 px-2">
+        Board 1
+      </button>
+      <button className="text-amber-50 h-full text-base pt-3 pb-1 px-2">
+        Board 1
+      </button>
+      <button className="text-amber-50 h-full text-base pt-3 pb-1 px-2">
+        Board 1
+      </button>
 
       <Tooltip placement="bottom" title="Add new board">
-        <button onClick={() => {openModal(MODALS.NEW_BOARD); setModalTitle("New Board Editor");}} className="text-amber-50 flex items-center ml-4">
+        <button
+          onClick={() => {
+            openModal(MODALS.NEW_BOARD);
+            setModalTitle("New Board Editor");
+          }}
+          className="text-amber-50 flex items-center ml-4"
+        >
           <PlusOutlined className="text-base" />
         </button>
       </Tooltip>
